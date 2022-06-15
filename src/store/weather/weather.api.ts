98e6d0304380e31b3ceaa5ecc4292c558f383f0c
webRofl <kakaxa1928@mail.ru>
2022-06-15 22:40:43 +0400

    fixed some bug

--- src/store/weather/weather.api.ts
+++ src/store/weather/weather.api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const weatherApi = createApi({
   reducerPath: 'api/weather',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://api.openweathermap.org' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org' }),
   endpoints: (build) => ({
     getWeather: build.query<IWeatherResponse, void>({
       query: () => ({
