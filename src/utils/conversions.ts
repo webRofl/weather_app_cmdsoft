3dd042501f8a317f15d24aa76ca13264ad7e89d9
webRofl <kakaxa1928@mail.ru>
2022-06-20 18:26:10 +0400

    ad

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -5,4 +5,4 @@ export const conversionKelvinToFahrenheit = (temp: number) =>
   Math.trunc(((temp - 273.15) * 9) / 5 + 32); // kelvin --> fahrenheit
 
 export const conversionPressure = (pressure: number) =>
-  Math.trunc(pressure / 1.333); // gPa --> mm. rt. st.
+  Math.trunc(pressure / 1.333); // gPa --> mm rt st
